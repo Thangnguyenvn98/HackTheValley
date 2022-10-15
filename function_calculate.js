97895feb51bcfb214d8362f005f7c50a98f9631c
Alan Pangnathip <92758882+apangnathip@users.noreply.github.com>
2022-10-15 13:57:44 -0400

    Update function_calculate.js

--- function_calculate.js
+++ function_calculate.js
@@ -1,21 +1,21 @@
-function calculate(key, inputBox, outputBox, fieldID, type, ifYearly) {
-    $w(outputBox).text = `Your carbon footprint by ${type} amounts to 0 tonnes yearly`
-    wixData.get('Emissions', key)
+function calcFootPrint(ifYearly, id, inputBox, outputBox, fieldID, type) {
+    $w(outputBox).text = `Your carbon footprint by ${type} amounts to 0 tonnes yearly`;
+    wixData.get('Emissions',(id)
         .then((item) => {
-            let factor = item.fieldID
-        })
-    $w('#inputBox').onInput(() => {
-        let time = parseFloat($w('inputBox').value)
+            let factor = item.fieldID;
+        }));
+    $w(`#{inputBox}`).onInput(() => {
+        let time = parseFloat($w(`#{inputBox}`).value);
         if(ifYearly) {
-            let emission = (time * factor) / 1000
+            let emission = (time * factor) / 1000;
         }
         else {
-            let emission = (time * factor * 52) / 1000
+            let emission = (time * factor * 52) / 1000;
         }
-        emission = Math.round(emission * 100) / 100
+        emission = Math.round(emission * 100) / 100;
         if(isNaN(emission)) {
-            emission = 0
+            emission = 0;
         }
-        $w(outputBox).text = `Your carbon footprint by ${type} amounts to ${emission} yearly`
-    })
+        $w(outputBox).text = `Your carbon footprint by ${type} amounts to ${emission} yearly`;
+    });
 }
